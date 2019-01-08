484fb36756c3ffc4877e7d9679c0c312b3ddbed7
christopheleray <christophe.leray1@gmail.com>
2019-01-08 08:41:12 +0100

    update item controller and partial for edit patch

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -6,8 +6,25 @@ module Administration
       @items = Item.sorted_by_price
     end
 
+    def edit
+      @item = Items.params[:id]
+    end
+
+
     def update
+      # puts "----je rentre dans update-----"
+      # params.inspect
+      # item = Item.find(params[:id])
+      # puts "---j affiche item----"
+      # puts params[:item][:discount_percentage]
+      # p item
+      # item.update_attributes(item_params)
       redirect_to administration_items_path
+    end
+
+  private
+    def item_params
+      params.require(:item).permit(:discount_percentage)
     end
   end
 end
