2047ce602333f69409edd8b2445b650111214c00
christopheleray <christophe.leray1@gmail.com>
2019-01-08 10:39:59 +0100

    update item controller with private method for better code

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -13,8 +13,26 @@ module Administration
 
     def update
       item = Item.find(params[:id])
-      puts "-----new discount percentage is : #{params[:item][:discount_percentage]}-----"
-      
+      update_item(item)
+      item_valid?(item)
+      redirect_to administration_items_path
+    end
+
+  private
+    def item_params
+      params.require(:item).permit(:discount_percentage)
+    end
+
+    def item_valid?(item)
+      if item.valid?
+        item.save
+        flash[:notice] = "Product updated successfuly" 
+        else 
+          flash[:alert] = item.errors.full_messages
+      end
+    end
+
+    def update_item(item)
       if params[:item][:discount_percentage] != 0
         item.update_attributes(item_params)
         item.has_discount = true
@@ -22,20 +40,6 @@ module Administration
           item.update_attributes(item_params)
           item.has_discount = false
       end
-      
-      if item.valid?
-        item.save
-        flash[:notice] = "Product updated successfuly" 
-        else 
-          flash[:alert] = item.errors.full_messages
-      end
-
-      redirect_to administration_items_path
-    end
-
-  private
-    def item_params
-      params.require(:item).permit(:discount_percentage)
     end
   end
 end
