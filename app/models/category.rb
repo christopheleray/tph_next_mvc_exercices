70d0a7c45022e397ac4a7ee96fdad6e6f02ffe2c
christopheleray <christophe.leray1@gmail.com>
2019-01-11 11:34:21 +0100

    update active record assosciation ok

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :category_item_connections
   has_many :items, through: :category_item_connections
+  validates :category, presence: true, length: { minimum: 3}
 end
