1acb4af24e1e14d5945e3b73977942a33174004d
christopheleray <christophe.leray1@gmail.com>
2019-01-16 15:56:45 +0100

    add view item show and index for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#landing_page'
+  root 'items#index'
   devise_for :admins
   devise_for :users, controllers: { registrations: 'users/registrations' }
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/create'
-  get 'categories/update'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/create'
+  # get 'categories/update'
+  resources :users
+  resources :items, only: %i[index show]
 
   namespace 'administration' do
     get '/', to: 'items#index'
-
     resources :items
-    resources :users
   end
 end
