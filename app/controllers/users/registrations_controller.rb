b746d364f01981899130d4c8f9b4160327908cf5
christopheleray <christophe.leray1@gmail.com>
2019-01-17 21:50:25 +0100

    remove after_create options

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
   # GET /resource/sign_up
   # def new
   #   super
